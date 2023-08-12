1b07bead18dc0bd5796dc76dbfde3de0f4f77a66
Paulo Trindade <135180429+PauloTrindad@users.noreply.github.com>
2023-08-12 17:48:47 -0300

    Xbrowser Commands

---
+++ xbrowser/xbrowser.sh
@@ -0,0 +1,5 @@
+# Remember: This Command Only Works If You Have Polaris Console
+open xbrowser
+# If You Don't Have Xbrowser, Execute THIS Command:
+compose install xbrowser # If You Use Compose Package.
+dnf install xbrowser # If You Use DNF Package (Works More In Fedora)
